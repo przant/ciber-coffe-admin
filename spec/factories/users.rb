4a8bc3f9306add8a371d410b9466091b27a9eb30
Antonio Pérez <apema2079@gmail.com>
2022-07-13 05:26:16 -0500

    Update User Factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,22 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "test_user#{n}@mail.com" }
+    password { '12345678' }
+    password_confirmation { '12345678' }
+
+    trait :with_name do
+      sequence(:name) { |n| "User#{n}" }
+    end
+
+    trait :with_fullname do
+      sequence(:name) { |n| "User#{n}" }
+      sequence(:last_name) { |n| "Surname#{n}" }
+    end
+
+    trait :with_full_info do
+      sequence(:name) { |n| "User#{n}" }
+      sequence(:last_name) { |n| "Surname#{n}" }
+      sequence(:nickname) { |n| "Alias#{n}" }
+    end
   end
 end
